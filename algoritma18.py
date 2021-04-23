5dff95e7991e5d2adb3a70a0a4ea56acfd48f5d9
Bertugvatansever <79909762+Bertugvatansever@users.noreply.github.com>
2021-04-23 18:37:32 +0300

    Add files via upload

---
+++ algoritma18.py
@@ -0,0 +1,17 @@
+sayı=int(input("1.sayıyı giriniz"))
+sayı2=int(input("2.sayıyı giriniz"))
+sayı3=int(input("3.sayıyı giriniz"))
+if sayı>sayı2 and sayı>sayı3:
+    print("en büyük sayı",sayı,"dır.")
+elif sayı2>sayı and sayı2>sayı3:
+    print("en büyük sayı",sayı2,"dır.")
+elif sayı3>sayı and sayı3>sayı2:
+    print("en büyük sayı",sayı3,"dır.")
+if sayı<sayı2 and sayı<sayı3:
+    print("en kücük sayı",sayı,"dır")
+elif sayı2<sayı and sayı2<sayı3:
+    print("en kücük sayı",sayı2,"dır")
+elif sayı3<sayı and sayı3<sayı2:
+    print("en kücük sayı",sayı3,"dır")
+
+                
